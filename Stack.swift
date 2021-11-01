e51e1416e75c63839e76647107884e1ef3257ae5
Sofia Rodriguez <sofiaromorales@gmail.com>
2021-10-31 22:55:58 -0400

    Stack

---
+++ Stack.swift
@@ -0,0 +1,47 @@
+//
+//  Stack.swift
+//  
+//
+//  Created by Sofia Rodriguez Morales on 31/10/21.
+//
+
+import Foundation
+
+struct Stack<T> {
+    var storage = [T]()
+    
+    public init() { }
+    public init(_ elements: [T]) {
+        self.storage = elements
+    }
+    
+    var isEmpty: Bool {
+        peek() == nil
+    }
+    func peek() -> T? {
+        storage.last
+    }
+    mutating func push(_ element: T) {
+        storage.append(T)
+    }
+    @discardableResult
+    mutating func pop() -> T {
+        storage.removeLast()
+    }
+}
+
+extension Stack: CustomStringConvertible {
+    var description: String {
+        """
+        ----top----
+        \(storage.map { "\($0)"}.reversed().joined(separator: "\n"))
+        ---end---
+        """
+    }
+}
+
+extension Stack: ExpressibleByArrayLiteral {
+    public init(arrayLiteral elements: Element...) {
+        storage = elements
+    }
+}
