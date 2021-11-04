5dfd53ff69efdfbf51da0daec4a7210e50d004ea
Sofía Rodríguez <sofiaromorales@gmail.com>
2021-11-03 23:43:47 -0400

    Time Complexity for Stack

--- Stack.swift
+++ Stack.swift
@@ -5,6 +5,19 @@
 //  Created by Sofia Rodriguez Morales on 31/10/21.
 //
 
+/*
+     Time Complexity
+  =======================
+||    Action    || Time  ||
+||=======================||
+|| Pop at top   ||  O(1) ||
+||--------------||-------||
+|| Pop last     ||  O(n) ||
+||--------------||-------||
+|| Push         ||  O(1) ||
+  =======================
+*/
+
 import Foundation
 
 struct Stack<T> {
